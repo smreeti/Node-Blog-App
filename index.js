7c384f5d6132669b7373c42120c0aa02413eb537
smriti.mool <mool.smreeti@gmail.com>
2022-10-14 14:55:57 -0400

    test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const router = require('./routes/routes.js');
 
@@ -9,6 +10,19 @@ app.use(router);
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.listen(4000, ()=>{
+app.listen(4000, () => {
     console.log("App is listening to port 4000");
 })
+
+//middle ware to handle the requests from Mongodb
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb+srv://smriti:mongodb@cluster0.v3tjgvx.mongodb.net/Employees_Rogers?retryWrites=true&w=majority',
+    { useUnifiedTopology: true, useNewUrlParser: true }, (error) => {
+        if (error) {
+            console.log(error);
+            console.log("Connection failed");
+        } else {
+            console.log("Connected to the database");
+        }
+    });
